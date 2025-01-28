7b8dd445f353d9706718db0629bc6bf482bf0c2c
xAbduallah <xconquerps@gmail.com>
2025-01-28 08:04:02 +0200

    first commit

--- src/Components/RoutesProtector.jsx
+++ src/Components/RoutesProtector.jsx
@@ -6,15 +6,13 @@ import { UserContext } from "../Contexts/UserContext"
 export default function RoutesProtector(props) {
     const { user } = useContext(UserContext);
     const isAuthenticated = user?.token.length > 64;
-    const componentName = props.children?.type?.name;
 
-    
+    const childType = props.children?.type;
+    const componentName = childType?.name;
+
     if (isAuthenticated && (componentName === "Login" || componentName === "Register" || componentName === "Forgetpassword")) {
         return <Navigate to="/" />;
     }
-
-    console.log("RoutesProtector: ", componentName);
-    console.log("isAuthenticated: ", isAuthenticated);
     
     if (!isAuthenticated && componentName === "ProductDetails") {
         return <Navigate to="/login" />;
