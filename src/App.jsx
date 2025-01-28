39a995eaf6a784e6b30e59e40c09935f23326a69
xAbduallah <xconquerps@gmail.com>
2025-01-28 07:41:16 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import Home from "./Components/home";
+import Home from "./Components/Home";
 import Footer from "./Components/Footer";
 import Register from "./Components/Register";
 import Login from "./Components/Login";
